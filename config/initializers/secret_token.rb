1ab0d7d29ec6c9a1365609f2dbf3d5c11f2706d9
Sam Podlogar <sam@podlogar.com>
2013-10-29 14:17:21 -0500

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+DemoApp::Application.config.secret_token = 'dc0178a4f1a33468a7be897e0e208306b52375901be3f8d590a918a75861e9f9cfdc2390c4ebd926dbb4f0ee183735cb53268d4307df2d60ddeb43297a426eb4'
